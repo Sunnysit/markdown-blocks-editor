a964e5e36ea6dc11119929ac08eacd939f615e04
Sunny Xue <sunny.sit26@gmail.com>
2020-04-05 12:29:18 -0700

    Update README.md file and intro info

--- src/assets/introductionInfo.js
+++ src/assets/introductionInfo.js
@@ -2,16 +2,65 @@ export const APP_INTRODUCTION = [
   {
     type: 'heading',
     sid: 'zasAtvkC8',
-    data: {
-      value: 'What is Markdown Blocks Editor?',
-      level: 1,
-    },
+    data: { value: 'Markdown Blocks Editor', level: 1 },
+  },
+  {
+    type: 'blockquote',
+    sid: '5mUDSt1OB',
+    data: { value: 'A block based live editor to create Markdown text file.' },
+  },
+  {
+    type: 'heading',
+    sid: '6SYL_LrHp',
+    data: { value: 'What is Markdown Blocks Editor?', level: 2 },
   },
   {
     type: 'paragraph',
-    sid: '1VW0rHTVR',
+    sid: 'kn8z_RYeX',
     data: {
-      value: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      value:
+        'Markdown Blocks Editor is an in-browser editor to generate Markdown text file. It wraps different types of Markdown elements into blocks. User can simply build a Markdown text file without  memorizing the Markdown syntax.',
+    },
+  },
+  {
+    type: 'heading',
+    sid: 'JoFwnB-UH',
+    data: { value: 'How can I start writing my own text?', level: 2 },
+  },
+  {
+    type: 'list',
+    sid: '-fM7QA-Ab',
+    data: {
+      value: [
+        'Click the Empty button located in top right corner.',
+        "Click 'OK' to empty this placeholder file.",
+        'Feel free to create your own Markdown file!',
+      ],
+      listType: 'ol',
+    },
+  },
+  {
+    type: 'heading',
+    sid: '1FROtp3Pv',
+    data: {
+      value: 'What types of elements does Markdown Blocks Editor suppor?',
+      level: 2,
+    },
+  },
+  {
+    type: 'list',
+    sid: 'ehb67qtIR',
+    data: {
+      value: [
+        'Heading',
+        'Paragraph',
+        'Blockquote',
+        'Link',
+        'List (Order list and Unordered list)',
+        'Code block',
+        'Image',
+      ],
+      listType: 'ul',
     },
   },
 ];
